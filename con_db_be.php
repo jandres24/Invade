9c85500e4ecf418edd5a927d74ee1546ef18c3c2
jandres24 <102499067+jandres24@users.noreply.github.com>
2022-03-30 18:11:13 -0400

    Add files via upload

---
+++ con_db_be.php
@@ -0,0 +1,10 @@
+<?php
+
+$conex = mysqli_connect("localhost","root","","login_register_db"); 
+    /*
+    if($conex){
+        echo 'Conexion exitosa';
+    }else{
+        echo 'No se pudo conectar a la BD';
+    }*/
+?>
